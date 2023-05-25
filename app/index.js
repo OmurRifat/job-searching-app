1825a0b7bb83d11a587d91e673f347b3d5987a87
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-05-25 22:42:15 +0600

    some changes added

--- app/index.js
+++ app/index.js
@@ -1,10 +1,35 @@
-import { View, Text } from 'react-native'
+import { SafeAreaView, Text } from 'react-native';
+import { useRouter, Stack } from 'expo-router';
+import { COLORS } from '../constants';
+
 
 const Home = () => {
+    const router = useRouter();
+
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView
+            style={ {
+                flex: 1,
+                backgroundColor: COLORS.lightWhite
+            } }
+        >
+            <Stack.Screen
+                options={ {
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerTitle: () => (
+                        <Text
+                            style={ {
+                                fontSize: 20,
+                                fontWeight: 'bold',
+                                color: COLORS.black,
+                                justifyContent: 'center',
+                            } }
+                        >Home</Text>
+                    )
+                } }
+            />
+        </SafeAreaView>
     )
 }
 
